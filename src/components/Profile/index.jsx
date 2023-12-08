acbae0f17014e185bd5669fe7ab71b6e4b8938dd
anthonymnf <anthonymnf30@gmail.com>
2023-12-08 16:39:09 -0300

    Upload in repository

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -11,12 +11,10 @@ export default function Profile(props) {
       <ProfileSection>{props.bio}</ProfileSection>
       <ProfileSection>{props.phone}</ProfileSection>
       <ProfileSection>{props.email}</ProfileSection>
-      <ProfileSection>
-        <div className={styles.links}>
-          <LinkBtn href={props.githubUrl}>GitHub</LinkBtn>
-          <LinkBtn href={props.linkedinUrl}>LinkedIn</LinkBtn>
-          <LinkBtn href={props.twitterUrl}>Twitter</LinkBtn>
-        </div>
+      <ProfileSection className={styles.links}>
+        <LinkBtn href={props.githubUrl}>GitHub</LinkBtn>
+        <LinkBtn href={props.linkedinUrl}>LinkedIn</LinkBtn>
+        <LinkBtn href={props.twitterUrl}>Twitter</LinkBtn>
       </ProfileSection>
     </div>
   );
