6e6cdedecbecbdbe12933af90652f1a63864078e
anthonymnf <anthonymnf30@gmail.com>
2023-12-08 16:27:57 -0300

    Melhor componetização e add props children

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,24 +1,23 @@
 import styles from "./style.module.css";
+import Title from "../Title";
+import ProfileSection from "./ProfileSection";
+import LinkBtn from "../LinkBtn";
 
 export default function Profile(props) {
   return (
     <div className={styles.container}>
       <img className={styles.avatar} src={props.avatar} alt={props.name} />
-      <h2 className={styles.name}>{props.name}</h2>
-      <div>{props.bio}</div>
-      <div>{props.phone}</div>
-      <div>{props.email}</div>
-      <div className={styles.links}>
-        <a href={props.githubUrl} target="_blank">
-          GitHub
-        </a>
-        <a href={props.linkedinUrl} target="_blank">
-          LinkedIn
-        </a>
-        <a href={props.twitterUrl} target="_blank">
-          Twitter
-        </a>
-      </div>
+      <Title>{props.name}</Title>
+      <ProfileSection>{props.bio}</ProfileSection>
+      <ProfileSection>{props.phone}</ProfileSection>
+      <ProfileSection>{props.email}</ProfileSection>
+      <ProfileSection>
+        <div className={styles.links}>
+          <LinkBtn href={props.githubUrl}>GitHub</LinkBtn>
+          <LinkBtn href={props.linkedinUrl}>LinkedIn</LinkBtn>
+          <LinkBtn href={props.twitterUrl}>Twitter</LinkBtn>
+        </div>
+      </ProfileSection>
     </div>
   );
 }
