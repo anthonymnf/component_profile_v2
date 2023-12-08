945f7a23d7cbc840aa1e7e1115541c0cd35df461
anthonymnf <anthonymnf30@gmail.com>
2023-12-08 16:04:50 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Profile from "./components/Profile";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Profile
+        avatar="https://randomuser.me/api/portraits/men/75.jpg"
+        name="John Doe"
+        bio="Full-stack javascript developer at Acme Inc."
+        email="john.doe@email.com"
+        phone="+5511987654321"
+        githubUrl="https://github.com"
+        linkedinUrl="https://linkedin.com"
+        twitterUrl="https://twitter.com"
+      />
+    </div>
+  );
+}
