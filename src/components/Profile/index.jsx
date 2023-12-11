5ba1ecba7669158507cefb38a976ec5eee5edf2f
anthonymnf <anthonymnf30@gmail.com>
2023-12-11 10:11:44 -0300

    Add states in folllowBrn

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -2,19 +2,27 @@ import styles from "./style.module.css";
 import Title from "../Title";
 import ProfileSection from "./ProfileSection";
 import LinkBtn from "../LinkBtn";
+import { useState } from "react";
 
-function handleClick(ev) {
-  console.log(ev);
-  alert("Você está seguindo este perfil!!");
-}
 export default function Profile(props) {
+  const [followText, setFolowText] = useState("Follow");
+  function handleClick(ev) {
+    if (followText == "Follow") {
+      alert("Você está seguindo este perfil!!");
+      setFolowText("Following");
+    } else {
+      alert("Você deixou de seguir este perfil!!");
+      setFolowText("Follow");
+    }
+  }
+
   return (
     <div className={styles.container}>
       <img className={styles.avatar} src={props.avatar} alt={props.name} />
       <Title>
         <span>{props.name}</span>
         <button className={styles.followBtn} onClick={handleClick}>
-          Follow
+          {followText}
         </button>
       </Title>
       <ProfileSection>{props.bio}</ProfileSection>
